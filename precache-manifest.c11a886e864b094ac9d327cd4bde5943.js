bf5983cdf973028122dbec6eb5d983f3caf9e410
unknown <tranttuan96@gmail.com>
2020-09-05 22:21:06 +0700

    Updates

---
+++ precache-manifest.c11a886e864b094ac9d327cd4bde5943.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294a503d439bff6c9ce67a2de6d9c413",
+    "url": "/DuAn_Booking_Movie_React_FE39/index.html"
+  },
+  {
+    "revision": "d6c04ba82ea3d1d8751c",
+    "url": "/DuAn_Booking_Movie_React_FE39/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "53bf3ecc087d5fbc1143",
+    "url": "/DuAn_Booking_Movie_React_FE39/static/css/main.2ba9857e.chunk.css"
+  },
+  {
+    "revision": "d6c04ba82ea3d1d8751c",
+    "url": "/DuAn_Booking_Movie_React_FE39/static/js/2.d55c8a92.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/DuAn_Booking_Movie_React_FE39/static/js/2.d55c8a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53bf3ecc087d5fbc1143",
+    "url": "/DuAn_Booking_Movie_React_FE39/static/js/main.063c8e17.chunk.js"
+  },
+  {
+    "revision": "f4ba7f7be4243bc4d0df",
+    "url": "/DuAn_Booking_Movie_React_FE39/static/js/runtime-main.1e68858c.js"
+  },
+  {
+    "revision": "d713fc3a3e00188abf478fc9ee7a9690",
+    "url": "/DuAn_Booking_Movie_React_FE39/static/media/back-cinemaList.d713fc3a.png"
+  },
+  {
+    "revision": "4c4a3b7b4af30fa742d2794a75d3196f",
+    "url": "/DuAn_Booking_Movie_React_FE39/static/media/bgLogin.4c4a3b7b.jpg"
+  }
+]);
